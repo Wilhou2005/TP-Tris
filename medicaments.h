656c9136ec9c4c7b7b4d14f734dbf37d6ce64c6a
Wilhelm Macaigne <wmacaigne@gmail.com>
2026-04-20 14:25:14 +0200

    premier commit .h

---
+++ medicaments.h
@@ -0,0 +1,22 @@
+#ifndef MEDICAMENT_H
+#define MEDICAMENT_H
+
+#define N 80
+
+typedef struct {
+    char nom[50];
+    int code;
+    int jf, mf, af;
+    int jp, mp, ap;
+    float prix;
+    int vendus;
+    int stock;
+} Medicament;
+
+void saisir(Medicament t[]);
+void tri(Medicament t[]);
+int recherche(Medicament t[], char nom[]);
+void plus_cher(Medicament t[]);
+float taux(Medicament t[]);
+
+#endif
